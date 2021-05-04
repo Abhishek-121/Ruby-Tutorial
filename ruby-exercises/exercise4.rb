dcb53f462181839df4b31f7dfbddb967957a4192
Abhishek Maheshwari <56202291+Abhishek-121@users.noreply.github.com>
2021-05-04 16:35:53 +0530

    Exercise from 1-4

---
+++ ruby-exercises/exercise4.rb
@@ -0,0 +1,24 @@
+# Exercise: 4 - Palindrome
+
+# Output whether the input string is a palindrome.Add a method palindrome? 
+# in String class, so that it can be called as str.palindrome? this method should return true/false. 
+# Input should be passed as a command line argument. * When the string is Palindrome, the output should be "Input string is a palindrome"
+
+# * When the string is not Palindrome, the output should be "Input string is not a palindrome"
+# * If no argument is passed(or blank string argument). the output should be "Please provide an input"
+
+# Code:
+input_string = gets.chomp 
+def check_pallindrone?(input_string)
+  if input_string == input_string.reverse
+    puts 'Input string is a palindrome'
+  else
+    puts 'Input string is not a palindrome'
+  end
+end
+
+if input_string == ''
+  puts 'Please provide an input'
+  input_string = gets.chomp
+end
+check_pallindrone?(input_string)
