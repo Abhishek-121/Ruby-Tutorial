ebeb973dd75e227287402deac6ba9180d6090e94
Abhishek Maheshwari <56202291+Abhishek-121@users.noreply.github.com>
2021-05-07 16:21:51 +0530

    exercise13-15

---
+++ ruby-exercises/exercise15.rb
@@ -0,0 +1,39 @@
+# Exercise: 15 Prime Numbers - Step
+
+# Define a method to find all prime numbers upto n using 'step' function. 
+# Don't use Prime class
+# [input]
+# 20
+# [/input]
+# [output]
+# [2, 3, 5, 7, 11, 13, 17, 19] 
+# [/output]
+# [input]
+# 100
+# [/input]
+# [output]
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97] 
+# [/output]
+
+# Code :
+
+def prime_number(n)
+  res = []
+  prime = Array.new(n + 1, true)
+  (2..n).each do |x|
+    break if x * x > n
+
+    if prime[x]
+      (x * x..n).step(x).each do |multiples|
+        prime[multiples] = false
+      end
+    end
+  end
+  (2..n).each do |primes|
+    res << primes if prime[primes]
+  end
+  res
+end
+
+num = ARGV[0].to_i
+print "#{prime_number(num)}\n"
