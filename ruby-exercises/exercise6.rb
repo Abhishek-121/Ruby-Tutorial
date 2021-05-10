b8d77559cd98223c649619f9d721fd25575f2af7
Abhishek Maheshwari <56202291+Abhishek-121@users.noreply.github.com>
2021-05-10 16:43:28 +0530

    Vehicle-subclass

--- ruby-exercises/exercise6.rb
+++ ruby-exercises/exercise6.rb
@@ -1,3 +1,23 @@
+# Exercise: 6 Vehicle - Subclass
+# Define a class named 'Vehicle' consisting of 'name','price' and methods for initializing and showing contents(overwrite to_s). 
+# The 'price' of Vehicle may change over time. Now create a subclass 'Bike' having 'dealer' and percent_price_increase and method to show its content. 
+# Initialize a Bike class object with certain values. Define a method price_increase which will increase the price. 
+# Your program should accept command line input in this format: BikeName Price Dealer PercentPriceIncrease. 
+# Ex: BajajDiscover 58000 BaggaLink 12
+# [input]
+# "Hero Honda" 65000 "Koncept Automobiles" 25 
+# [output]
+# Bike Name: Hero Honda
+# Bike Price: 65000
+# Bike Dealer: Koncept Automobiles
+
+# After 25.0 percent hike in price:
+# Bike Name: Hero Honda
+# Bike Price: 81250.0
+# Bike Dealer: Koncept Automobiles 
+
+# Code : 
+
 class Vehicle
   def initialize(name, price)
     @name = name
