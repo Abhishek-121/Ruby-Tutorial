fa03f2d7fe796979ea7600ce8d32a5c04dff0d5d
Abhishek Maheshwari <56202291+Abhishek-121@users.noreply.github.com>
2021-05-16 20:19:50 +0530

    occurence hash

--- ruby-exercises/exercise1.rb
+++ ruby-exercises/exercise1.rb
@@ -19,5 +19,4 @@ input_string = ARGV[0]
 def count_occurence(input_string)
   input_string.chomp.gsub!(/\W+|\d+/, '').chars.group_by { |chr| chr }.transform_values(&:size)
 end
-
 puts count_occurence(input_string)
