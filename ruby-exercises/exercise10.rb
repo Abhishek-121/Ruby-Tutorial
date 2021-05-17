c49d5906f6003ab25e61ba0211528ba3f8930fd3
Abhishek Maheshwari <56202291+Abhishek-121@users.noreply.github.com>
2021-05-17 16:04:43 +0530

    parsing and returning hash of even-odd done

--- ruby-exercises/exercise10.rb
+++ ruby-exercises/exercise10.rb
@@ -1,29 +1,10 @@
-def hash_count(input_hash)
-  # res = {}
-  # input_hash.each do |x|
-  #   key = x.length
-  #   if res[key]
-  #     res[key].push(x)
-  #   else
-  #     res[key] = [x]
-  #   end
-  # end
-  input_hash.inject({:odd=[], :even=[]}) do |k, v|
-    k[:odd] << v if v.length%2
-    k[:even] << v if v.length%2 == 0
+def hash_count(input_list)
+  input_list.inject({odd: [], even: []}) do |res, v|
+    res[:odd] << v if v.length.odd?
+    res[:even] << v if v.length.even?
+    res
   end
-  put k
-  
-  # res = res.sort.to_h
-  # res.inject({}) do |result, element|
-  #   if element[0].to_i % 2
-  #     result[element[0].to_i = 'odd'] = element.last
-  #   else
-  #     result[element[0].to_i = 'even'] = element.last
-  #   end
-  # end
-  # puts res
 end
 
-input_hash = gets.chomp.strip.gsub!(/"|'|\[|\]/, '').split(',')
-hash_count(input_hash)
+input_list = gets.chomp.strip.gsub!(/"|'|\[|\]/, '').split(',')
+puts hash_count(input_list)
--- ruby-exercises/exercise17.rb
+++ ruby-exercises/exercise17.rb
@@ -1,15 +1,15 @@
 # Exercise: 17 Sum Time
 # Write a method that sums up 2 24-hour time values(h:m:s).
 # Validate the time using regular expressions.
-# Eg: ("0:45:34" "0:15:58") -> 01:01:32 ; ("11:23:07" "22:53:45") -> 1 day & 10:16:52.
+# Eg: ("0:45:34","0:15:58") -> 01:01:32 ; ("11:23:07","22:53:45") -> 1 day & 10:16:52.
 # Note: Make use of Time class
 
 require 'time'
 
-def sum_time(hour1, min1, sec1, hour2, min2, sec2)
-  
-  time1_in_seconds = hour1 * 3600 + min1 * 60 + sec1
-  time2_in_seconds = hour2 * 3600 + min2 * 60 + sec2
+def sum_time(time1, time2)
+ 
+  time1_in_seconds = time1.hour * 3600 + time1.min * 60 + time1.sec
+  time2_in_seconds = time2.hour * 3600 + time2.min * 60 + time2.sec
   total_sec = time1_in_seconds + time2_in_seconds
   days = total_sec/86400
   remain_sec = total_sec % 86400
@@ -20,20 +20,16 @@ end
 
 TIME_PATTERN = /^([0-2]?[0-3]|[0-1]?[0-9]):([0-5][0-9]):([0-5][0-9])/
 
-input = ARGV
+input_time = ARGV
+input = input_time[0].split(",")
+
 t1 = TIME_PATTERN.match(input[0])
 t2 = TIME_PATTERN.match(input[1])
 
 if t1 && t2
-  time1 = input[0].gsub(/"/, '').split(':')
-  time2 = input[1].gsub(/"/, '').split(':')
-  hour1 = time1[0].to_i
-  min1 = time1[1].to_i
-  sec1 = time1[2].to_i
-  hour2 = time2[0].to_i
-  min2 = time2[1].to_i
-  sec2 = time2[2].to_i
-  puts sum_time(hour1, min1, sec1, hour2, min2, sec2)
+  time1 = Time.parse(input[0])
+  time2 = Time.parse(input[1])
+  puts sum_time(time1, time2)
 else
   puts 'Invalid 24-hour time value'
 end
