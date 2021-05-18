eb5cf34ba293b2d94af40d7437bce4c3783382da
Abhishek Maheshwari <56202291+Abhishek-121@users.noreply.github.com>
2021-05-18 16:21:55 +0530

    handling invalid time

--- ruby-exercises/exercise19.rb
+++ ruby-exercises/exercise19.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 # Exercise: 19 Sum Time
 
-# Modify the sum of times method for any number of time values passed to this method. 
-# Eg: ("11:23:07","22:53:45","0:23:23","23:45:56") -> "2 day & 10:26:11" 
+# Modify the sum of times method for any number of time values passed to this method.
+# Eg: ("11:23:07","22:53:45","0:23:23","23:45:56") -> "2 day & 10:26:11"
 #     ("11:23:07") -> "11:23:07"
 
 # [input]
 # "0:45:34","0:15:58"
 # [/input]
 # [output]
-# "01:01:32" 
+# "01:01:32"
 # [/output]
-# [input] 
+# [input]
 # "11:34:23","2:23:21","11:12-34"
 # [/input]
 # [output]
@@ -22,12 +24,7 @@
 require 'time'
 
 def sum_time(list)
-  list.map!{ |tt| tt.hour * 3600 + tt.min * 60 + tt.sec }
-  puts list
-  total_sec = 0 
-  list.each do |sec|
-    total_sec += sec
-  end
+  total_sec = list.map { |a| a }.map! { |tt| tt.hour * 3600 + tt.min * 60 + tt.sec }.sum
   days = total_sec / 86_400
   remain_sec = total_sec % 86_400
   time = Time.at(remain_sec).utc.strftime('%T')
@@ -35,19 +32,14 @@ def sum_time(list)
   "#{day}#{time}"
 end
 
-TIME_PATTERN = /^([0-2]?[0-3]|[0-1]?[0-9]):([0-5][0-9]):([0-5][0-9])/
+TIME_PATTERN = /^([0-2]?[0-3]|[0-1]?[0-9]):([0-5][0-9]):([0-5][0-9])/.freeze
 
 input_time = ARGV
-input = input_time[0].split(",")
+input = input_time[0].split(',')
 
-# t1 = TIME_PATTERN.match(input[0])
-# t2 = TIME_PATTERN.match(input[1])
-flag = 1
-input.each{ |t| flag = 0 unless TIME_PATTERN.match(t)}
-# if t1 && t2
-if flag
-  input = input.map{ |time| Time.parse(time)}
+begin
+  input = input.map { |time| Time.parse(time) }
   puts sum_time(input)
-else
+rescue ArgumentError => e
   puts 'Invalid 24-hour time value'
 end
