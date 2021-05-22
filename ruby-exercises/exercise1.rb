8754f29391d12c2709fc38a11cb0de003c39db1a
Abhishek Maheshwari <56202291+Abhishek-121@users.noreply.github.com>
2021-05-22 23:51:58 +0530

    occurence hash

--- ruby-exercises/exercise1.rb
+++ ruby-exercises/exercise1.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Exercise: 1 - Occurrence Hash
 # Count the occurrences of various alphabet letters in an input string and store it in hash.
 # Your ruby program should accept a string as an argument and display the hash as an output.
@@ -14,9 +12,9 @@
 
 # Code :
 
-input_string = ARGV[0]
-
 def count_occurence(input_string)
   input_string.chomp.gsub!(/\W+|\d+/, '').chars.group_by { |chr| chr }.transform_values(&:size)
 end
+
+input_string = ARGV[0]
 puts count_occurence(input_string)
